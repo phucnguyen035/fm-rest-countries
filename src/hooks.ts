a068263af52e75e352d029b2f79213991a794c32
Phuc Nguyen <40336551+phucnguyen035@users.noreply.github.com>
2022-04-11 00:59:01 +0700

    feat: add handle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,9 +1,27 @@
-import type { GetSession } from '@sveltejs/kit';
-import { parse } from 'cookie';
+import type { GetSession, Handle } from '@sveltejs/kit';
+import { parse, serialize } from 'cookie';
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const response = await resolve(event);
+
+  const cookies = parse(event.request.headers.get('cookie') || '');
+
+  if (!cookies.theme) {
+    const themeCookie = serialize('theme', 'dark', {
+      httpOnly: true,
+      secure: import.meta.env.PROD,
+      sameSite: 'strict',
+    });
+
+    response.headers.append('set-cookie', themeCookie);
+  }
+
+  return response;
+};
 
 export const getSession: GetSession = ({ request }) => {
   const cookie = request.headers.get('cookie') ?? '';
-  // const { theme = 'test' } = parse(cookie);
+  const { theme = 'dark' } = parse(cookie);
 
-  return { theme: JSON.stringify(parse(cookie)) as App.Session['theme'] };
+  return { theme: theme as App.Session['theme'] };
 };
