b74278d2004048aa1f5ab814123ed0006b206dcb
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2022-04-11 19:51:47 +0700

    feat(components): add country card component

---
+++ src/lib/components/CountryCard.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { Country } from '$lib/stores/country';
+
+  export let country: Country;
+</script>
+
+<div class="h-full overflow-hidden rounded-lg shadow dark:bg-elements">
+  <img
+    src={country.flag}
+    alt={`Flag of ${country.name}`}
+    height="208"
+    class="h-52 w-full object-cover"
+  />
+
+  <div class="space-y-4 p-8">
+    <h4 class="text-lg font-bold">{country.name}</h4>
+
+    <div class="leading-relaxed">
+      <p><span class="font-semibold">Population:</span> {country.population}</p>
+      <p><span class="font-semibold">Region:</span> {country.region}</p>
+      <p><span class="font-semibold">Capital:</span> {country.capital}</p>
+    </div>
+  </div>
+</div>
