067f20bcba2d58f294b25deb39d6eef22d5ee944
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2022-04-11 19:52:29 +0700

    feat(routes): add margin bottom from header in layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,7 +6,7 @@
 
 <div id="app" class={$theme}>
   <div class="min-h-screen bg-background text-text transition-colors">
-    <header class="h-16 shadow-md dark:bg-elements dark:shadow">
+    <header class="mb-8 h-16 shadow-md dark:bg-elements dark:shadow">
       <div class="container flex h-full items-center justify-between">
         <h1 class="lg:text-xl font-semibold">
           <a href="/">Where in the world?</a>
