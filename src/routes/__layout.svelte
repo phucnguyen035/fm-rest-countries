2bcadb1ace760fded8bb7b13855f94af4dc80e16
Phuc Nguyen <40336551+phucnguyen035@users.noreply.github.com>
2022-04-18 20:21:26 +0700

    feat: add nprogress

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,23 @@
 <script>
+  import NProgress from 'nprogress';
+  import { navigating } from '$app/stores';
   import Footer from '$lib/components/Footer.svelte';
   import Header from '$lib/components/Header.svelte';
   import { theme } from '$lib/stores/theme';
+
   import '../app.css';
+  import 'nprogress/nprogress.css';
+
+  NProgress.configure({ minimum: 0.16 });
+
+  $: {
+    if ($navigating) {
+      NProgress.start();
+    }
+    if (!$navigating) {
+      NProgress.done();
+    }
+  }
 </script>
 
 <div id="app" class={$theme}>
