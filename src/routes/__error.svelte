9677c7c916a2b9c8b677c4a63e514ff77eba8fbd
Phuc Nguyen <40336551+phucnguyen035@users.noreply.github.com>
2022-04-18 11:58:26 +0700

    feat(routes): add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<div class="mb-4 space-y-2">
+  <h1 class="text-2xl font-semibold text-[tomato]">{$page.status} - {$page.error.message}</h1>
+  {#if $page.error.stack}
+    <pre>{$page.error.stack}</pre>
+  {/if}
+</div>
+
+<a href="/" class="inline-block rounded bg-elements p-4 shadow">Go back</a>
