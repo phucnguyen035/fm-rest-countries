e6351027a316121a5b8f56d5adce3b013028456a
Phuc Nguyen <40336551+phucnguyen035@users.noreply.github.com>
2022-04-18 15:03:49 +0700

    feat(api): add get country and get bordering countries api

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -32,10 +32,42 @@ export async function getAllCountries(type: string | null, query: string | null)
   return res.json();
 }
 
-export type CountryDetail = Pick<Country, 'name' | 'population' | 'capital'> & {
+export type CountryDetail = Pick<Country, 'name' | 'population' | 'capital' | 'flag'> & {
+  topLevelDomain: string[];
+  nativeName: string;
+  region: string;
+  subregion: string;
+  borders?: string[];
+  currencies: Array<{ code: string; name: string; symbol: string }>;
+  languages: Array<{ iso639_1: string; iso639_2: string; name: string; nativeName: string }>;
+};
 
-  const res = await fetch(url.toString());
-  const data = await res.json();
+export async function getCountry(alpha2Code: string): Promise<CountryDetail | null> {
+  const fields = [
+    'name',
+    'population',
+    'capital',
+    'flag',
+    'topLevelDomain',
+    'nativeName',
+    'region',
+    'subregion',
+    'borders',
+    'currencies',
+    'languages',
+  ].join(',');
+  const res = await callApi(`alpha/${alpha2Code}?fields=${fields}`);
 
-  return data;
+  return res.ok ? res.json() : null;
+}
+
+export type Neighbor = Pick<Country, 'name' | 'flag' | 'alpha2Code'>;
+
+export async function getBorderingCountries(borderArray: string[]): Promise<Neighbor[]> {
+  const codes = borderArray.map((c) => c.toLowerCase()).join(',');
+  const fields = ['name', 'flag', 'alpha2Code'].join(',');
+
+  const res = await callApi(`alpha/?codes=${codes}&fields=${fields}`);
+
+  return res.json();
 }
