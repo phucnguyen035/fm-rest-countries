ff0ff0a4b087c55eef8aa6da81ff62250b4f4f15
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2022-12-16 15:27:51 +0700

    refactor(routes): rename layout and errors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,31 @@
 <script>
+  import NProgress from 'nprogress';
+  import { navigating } from '$app/stores';
+  import Footer from '$lib/components/Footer.svelte';
+  import Header from '$lib/components/Header.svelte';
+  // import { theme } from '$lib/stores/theme';
+
   import '../app.postcss';
+  import 'nprogress/nprogress.css';
+
+  NProgress.configure({ minimum: 0.16 });
+
+  $: {
+    if ($navigating) {
+      NProgress.start();
+    }
+    if (!$navigating) {
+      NProgress.done();
+    }
+  }
 </script>
 
-<slot />
+<div id="app" class="dark">
+  <div class="flex min-h-screen flex-col bg-background text-text transition-colors">
+    <Header />
+    <main class="container flex-grow">
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</div>
