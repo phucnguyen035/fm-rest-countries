542eac666d4c9ec1af41214893c8733feaa74378
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2022-12-17 18:29:40 +0700

    tests: add save theme test

--- tests/test.ts
+++ tests/test.ts
@@ -14,9 +14,20 @@ test.describe('Home page', () => {
     await expect(app).toHaveClass('light');
 
     await toggleBtn.click();
+    await expect(app).toHaveClass('dark');
+  });
+
+  test('Save theme preference across page reloads', async ({ page }) => {
+    const toggleBtn = page.locator(`button[aria-labelledby="dark-mode"]`);
+    const app = page.locator('#app');
+
+    await expect(toggleBtn).toBeVisible();
     await expect(app).toHaveClass('light');
 
     await toggleBtn.click();
+    await expect(app).toHaveClass('dark');
+
+    await page.reload();
     await expect(app).toHaveClass('dark');
   });
 
@@ -33,6 +44,15 @@ test.describe('Home page', () => {
     const AMOUNT_OF_COUNTRIES = 40;
     const countries = page.locator('ul#countries > li');
 
+    await expect(countries).toHaveCount(AMOUNT_OF_COUNTRIES);
+  });
+
+  test('should be able to search', async ({ page }) => {
+    const AMOUNT_OF_COUNTRIES = 1;
+    const searchInput = page.locator('input#search');
+    const countries = page.locator('ul#countries > li');
+
+    await searchInput.fill('United States of America');
     await expect(countries).toHaveCount(AMOUNT_OF_COUNTRIES);
   });
 });
