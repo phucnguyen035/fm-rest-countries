f864be2b0e71525694ea849be7e2775e35e2a42f
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2022-12-17 10:10:38 +0700

    feat: enhance error page typing

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,14 +2,16 @@
   import { dev } from '$app/environment';
   import { page } from '$app/stores';
 
-  $: error = $page.error as Error;
+  $: error = $page.error;
 </script>
 
-<div class="mb-4 space-y-2">
-  <h2 class="text-2xl font-semibold text-[tomato]">{$page.status} - {error.message}</h2>
-  {#if dev && error.stack}
-    <pre>{error.stack}</pre>
-  {/if}
-</div>
+{#if error}
+  <div class="mb-4 space-y-2">
+    <h2 class="text-2xl font-semibold text-[tomato]">{$page.status} - {error.message}</h2>
+    {#if dev && error.stack}
+      <pre>{error.stack}</pre>
+    {/if}
+  </div>
+{/if}
 
 <a href="/" class="inline-block rounded bg-elements p-4 shadow">Go back</a>
