94e469756eefe7168c3ca557d8869a79799394ac
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2022-12-17 10:10:58 +0700

    feat: add schemas, constants, parse utils

---
+++ src/lib/schemas/index.ts
@@ -0,0 +1,41 @@
+import { z } from 'zod';
+
+export const Country = z.object({
+  name: z.string(),
+  population: z.number(),
+  region: z.string(),
+  capital: z.optional(z.string()),
+  flag: z.string(),
+  alpha2Code: z.string(),
+  independent: z.boolean(),
+});
+export type Country = z.infer<typeof Country>;
+
+export const CountryDetail = Country.pick({
+  name: true,
+  population: true,
+  capital: true,
+  flag: true,
+}).extend({
+  topLevelDomain: z.array(z.string()),
+  nativeName: z.string(),
+  region: z.string(),
+  subregion: z.string(),
+  borders: z.optional(z.array(z.string())),
+  currencies: z.array(
+    z.object({
+      code: z.string(),
+      name: z.string(),
+      symbol: z.string(),
+    }),
+  ),
+  languages: z.array(
+    z.object({
+      iso639_1: z.string(),
+      iso639_2: z.string(),
+      name: z.string(),
+      nativeName: z.string(),
+    }),
+  ),
+});
+export type CountryDetail = z.infer<typeof CountryDetail>;
---
+++ src/lib/utils/parse.ts
@@ -0,0 +1,18 @@
+import { error } from '@sveltejs/kit';
+import type { z } from 'zod';
+
+const handleParseResult = <Input, Output>(result: z.SafeParseReturnType<Input, Output>) => {
+  if (!result.success) {
+    throw error(500, { message: 'Error parsing object', stack: result.error.stack });
+  }
+
+  return result.data;
+};
+
+export const parseJsonObject = async <O extends z.AnyZodObject>(object: O, json: unknown) => {
+  return handleParseResult(await object.safeParseAsync(json));
+};
+
+export const parseJsonArray = async <O extends z.AnyZodObject>(object: O, json: unknown) => {
+  return handleParseResult(await object.array().safeParseAsync(json));
+};
