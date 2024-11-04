8516a5d494ec7f2583d94df71638da86efb55b28
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-11-04 10:51:25 +0700

    test: update detail page test

--- tests/detail.spec.ts
+++ tests/detail.spec.ts
@@ -6,30 +6,31 @@ test.describe('Detail page', () => {
   });
 
   test('should display country page with correct information', async ({ page }) => {
-    await expect(page.getByRole('heading', { exact: true, name: 'Vietnam' })).toBeVisible();
-    await expect(page.getByText('Native Name: Việt Nam')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Vietnam', exact: true })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Native Names' })).toContainText('Việt Nam');
 
-    const population = await page.locator('h3:has-text("Population:") ~ span').innerText();
-    const populationNumber = parseInt(population.replaceAll(',', ''));
+    const population = await page.getByRole('heading', { name: 'Population' }).innerText();
+    const populationNumber = parseInt(population.replaceAll(',', '').split(' ')[1]);
     expect(populationNumber).toBeGreaterThan(90_000_000);
 
-    await expect(page.getByText('Region: Asia')).toBeVisible();
-    await expect(page.getByText('Sub region: South-Eastern Asia')).toBeVisible();
-    await expect(page.getByText('Languages: Vietnamese')).toBeVisible();
-    await expect(page.getByText('Capital: Hanoi')).toBeVisible();
-    await expect(page.getByText('Top level domain: .vn')).toBeVisible();
-    await expect(page.getByText('Currency: Vietnamese đồng')).toBeVisible();
-
-    const borderCountriesAmount = 3; // Laos, Cambodia, China
-    await expect(page.locator('h2:has-text("Border countries:") ~ ul > li')).toHaveCount(
-      borderCountriesAmount,
+    await expect(page.getByRole('heading', { name: /^Region/ })).toContainText('Asia');
+    await expect(page.getByRole('heading', { name: 'Sub region' })).toContainText(
+      'South-Eastern Asia',
     );
+    await expect(page.getByRole('heading', { name: 'Languages' })).toContainText('Vietnamese');
+    await expect(page.getByRole('heading', { name: 'Capital' })).toContainText('Hanoi');
+    await expect(page.getByRole('heading', { name: 'Top level domain' })).toContainText('.vn');
+    await expect(page.getByRole('heading', { name: 'Currency' })).toContainText('Vietnamese đồng');
+    await expect(
+      page
+        .getByRole('region', { name: 'Border countries' })
+        .getByRole('list')
+        .getByRole('listitem'),
+    ).toHaveCount(3);
   });
 
   test('should navigate to a neighboring country ', async ({ page }) => {
-    const locator = page.getByText(/^China$/);
-    await locator.click();
-
+    await page.getByRole('link', { name: 'China' }).click();
     await expect(page.getByRole('heading', { name: 'China', exact: true })).toBeVisible();
   });
 });
