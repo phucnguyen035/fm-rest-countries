5c7c0d2f11ab96ff8269ccf8aa7c2b664ae96e0e
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-11-04 10:50:21 +0700

    refactor: update default theme

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,7 @@
 import { ThemeEnum } from '$lib/stores/theme';
 import { enum_, optional, parse } from 'valibot';
 
-const ThemeSchema = optional(enum_(ThemeEnum), ThemeEnum.DARK);
+const ThemeSchema = optional(enum_(ThemeEnum), ThemeEnum.LIGHT);
 
 export function load({ cookies }) {
   const initialTheme = parse(ThemeSchema, cookies.get('theme'));
