115928057daa10343aa12ae86a0842ad56ed9c69
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-12-24 13:15:46 +0700

    refactor: update error page to svelte 5

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,13 @@
-<script lang="ts">
+<script>
   import { dev } from '$app/environment';
-  import { page } from '$app/stores';
-
-  $: error = $page.error;
+  import { page } from '$app/state';
 </script>
 
-{#if error}
+{#if page.error}
+  {@const { error, status } = page}
+
   <div class="mb-4 space-y-2">
-    <h2 class="text-2xl font-semibold text-[tomato]">{$page.status} - {error.message}</h2>
+    <h2 class="text-2xl font-semibold text-[tomato]">{status} - {error.message}</h2>
     {#if dev && error.stack}
       <pre>{error.stack}</pre>
     {/if}
