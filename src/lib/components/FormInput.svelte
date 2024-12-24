a4d1b47afe0e1f532c630d886e0e7497b432d652
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-12-24 16:25:53 +0700

    refactor: update form input to runes

--- src/lib/components/FormInput.svelte
+++ src/lib/components/FormInput.svelte
@@ -1,39 +1,37 @@
 <script lang="ts">
-  import { cx } from 'classix';
-
-  let className = '';
-
-  export let value: string | null;
-  export let label: string;
-  export let name: string;
-  export let type = 'text';
-  export let placeholder = '';
-  export { className as class };
-
-  const setType = (node: HTMLInputElement) => {
-    node.type = type;
+  type Props = {
+    value: string | null;
+    label: string;
+    name: string;
+    type: string;
+    placeholder: string;
+    class: string;
   };
+
+  let {
+    value = $bindable(''),
+    label,
+    name,
+    class: className = '',
+    type = 'text',
+    placeholder = '',
+  }: Props = $props();
+
+  function setType(node: HTMLInputElement) {
+    node.type = type;
+  }
 </script>
 
 <div class={className}>
   <label for={name} class="sr-only">{label}</label>
   <div class="relative rounded-md border-0 bg-elements shadow">
-    {#if $$slots.icon}
-      <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-6">
-        <slot name="icon" />
-      </div>
-    {/if}
-
     <input
       bind:value
       use:setType
       id={name}
       {name}
       {placeholder}
-      class={cx(
-        'sm:text-sm block w-full rounded-md border-0 bg-elements py-4 pl-14',
-        $$slots.icon && 'pr-8',
-      )}
+      class={'sm:text-sm block w-full rounded-md border-0 bg-elements px-8 py-4'}
     />
   </div>
 </div>
